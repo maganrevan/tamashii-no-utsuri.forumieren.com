bef4044511c02bc79852dff78a46561669b5b223
maganrevan <magnus.kruschwitz@outlook.com>
2015-06-09 09:15:47 +0200

    minor changes
    change the hierarchy, add if-statments

--- global.js
+++ global.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
-    $('<div><span id="counter"></span>&nbsp;<span id="words"></span></div>').appendTo('.sceditor-container');
     setInterval(function(){
-        var iCounter = $('.sceditor-container').children('textarea').val().split(/[\s,]+/).length-1;
-        $('#counter').html(iCounter);
-        if(iCounter === 1){
-            $('#words').html('Wort');
-        }
-        else{
-            $('#words').html('W&ouml;rter');
+        if($.type($('.sceditor-container').children('textarea').val()) !== "undefined"){
+            if($('#counter').length === 0){
+                $('<div><span id="counter"></span>&nbsp;<span id="words"></span></div>').appendTo('.sceditor-container');
+            }
+            var iCounter = $('.sceditor-container').children('textarea').val().split(/[\s,]+/).length-1;
+            $('#counter').html(iCounter);
+            if(iCounter === 1){
+                $('#words').html('Wort');
+            }
+            else{
+                $('#words').html('W&ouml;rter');
+            }
         }
     },500);
 });
