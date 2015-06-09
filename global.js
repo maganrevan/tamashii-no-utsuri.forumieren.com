c5d69f7c3c2e4f550dbd78b94a5bc72fa9dad4db
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2015-06-09 21:09:35 +0200

    Major-Bugfix

--- global.js
+++ global.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
     setInterval(function(){
         if($.type($('.sceditor-container').children('textarea').val()) !== "undefined"){
+        
             if($('#counter').length === 0){
                 $('<div><span id="counter"></span>&nbsp;<span id="words"></span></div>').appendTo('.sceditor-container');
             }
-			if(!$('.sceditor-container').children('textarea').val()){
-				var iCounter === 0;
+            
+            var sCurrent = $('.sceditor-container').children('textarea').val();
+			if(!sCurrent.trim()){
+				var iCounter = 0;
 			}
 			else{
-				var iCounter = $('.sceditor-container').children('textarea').val().split(/[\s,]+/).length;
+				var iCounter = sCurrent.split(/[\s,]+/).length;
 			}
             
             $('#counter').html(iCounter);
+            
             if(iCounter === 1){
                 $('#words').html('Wort');
             }
