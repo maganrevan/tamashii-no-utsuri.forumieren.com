db14b57c54310cc613f2d38ab7345844aa708e44
maganrevan <magnuskruschwitz@Magnus-Mac-mini.fritz.box>
2016-01-10 22:59:46 +0100

    Add $.bind on input
    Add the jQuery bind on the input in the textarea

--- global.js
+++ global.js
@@ -1,27 +1,29 @@
-$(document).ready(function(){
-    setInterval(function(){
-        if($.type($('.sceditor-container').children('textarea').val()) !== "undefined"){
-        
-            if($('#counter').length === 0){
-                $('<div><span id="counter"></span>&nbsp;<span id="words"></span></div>').appendTo('.sceditor-container');
+$(document).ready(function () {
+    setTimeout(function () {
+        if ($('#counter').length === 0) {
+            $('<div><span id="counter"></span>&nbsp;<span id="words"></span></div>').appendTo('.sceditor-container');
+        }
+
+        $('.sceditor-container').children('textarea').bind('keyup', function () {
+            var sCurrent = $(this).val();
+            var aCurrent = sCurrent.split(/[\s,]+/);
+
+            if (!sCurrent.trim()) {
+                var iCounter = 0;
+            } else {
+                var iCounter = sCurrent.split(/[\s,]+/).length;
             }
-            
-            var sCurrent = $('.sceditor-container').children('textarea').val();
-			if(!sCurrent.trim()){
-				var iCounter = 0;
-			}
-			else{
-				var iCounter = sCurrent.split(/[\s,]+/).length;
-			}
-            
+            if (aCurrent[aCurrent.length - 1] === '' && iCounter > 0) {
+                iCounter = iCounter - 1;
+            }
+
             $('#counter').html(iCounter);
-            
-            if(iCounter === 1){
+
+            if (iCounter === 1) {
                 $('#words').html('Wort');
-            }
-            else{
+            } else {
                 $('#words').html('W&ouml;rter');
             }
-        }
-    },500);
+        });
+    }, 1500);
 });
